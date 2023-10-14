28083f89fc077abf73e213ae7f26d7dfe7ee03b5
muhammaddaffafauzan <mobileasia215@gmail.com>
2023-10-14 21:28:07 +0700

    vercel

--- config/Database.js
+++ config/Database.js
@@ -2,7 +2,7 @@ import { Sequelize } from "sequelize";
 
 const db = new Sequelize({
     host: process.env.DB_HOST,
-    user: process.env.DB_USER,
+    user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     waitForConnections: true,
