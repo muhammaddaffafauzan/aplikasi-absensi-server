b32f913c56a7e1cb67e95ba27e6c03bab84900e3
muhammaddaffafauzan <mobileasia215@gmail.com>
2023-10-14 21:26:17 +0700

    vercel-done

--- config/Database.js
+++ config/Database.js
@@ -1,8 +1,13 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('absensi_db', 'root', '', {
-    host: 'localhost',
-    dialect: 'mysql',
+const db = new Sequelize({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queuelimit: 0,
 });
 
 export default db;
