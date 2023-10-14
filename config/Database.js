0e9635211da7b500f69ae00eabdae41e6bdd8d51
muhammaddaffafauzan <mobileasia215@gmail.com>
2023-10-14 21:40:10 +0700

    coba ini

--- config/Database.js
+++ config/Database.js
@@ -1,13 +1,8 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize({
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queuelimit: 0,
+    dialect: 'mysql',
 });
 
 export default db;
